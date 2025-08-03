10f5642ba1e28015ffd85f03e0a0cb455f919baa
Andrei Makarov <levi2ki1988@gmail.com>
2025-08-03 12:26:47 +0300

    fix(ci): fix github ci and some lint errors;

--- packages/rbac-core/src/lib/registry.ts
+++ packages/rbac-core/src/lib/registry.ts
@@ -1,21 +1,22 @@
-import { Module } from "./module";
+import { Module } from './module';
 
 declare type IfDef<LEFT, True, False> = [LEFT] extends [never] ? False : True;
 declare type KeysIntersection<A, B> = keyof A & keyof B;
 
-export interface Registry<Context extends { [x: string]: Module<any, string>}> {
+export interface Registry<Context extends { [x: string]: Module<any, string> }> {
     readonly modules: Context;
 }
 
-
-export function getDefaultRegistry(): Registry<{}> {
+export function getDefaultRegistry(): Registry<NonNullable<unknown>> {
     return { modules: {} };
 }
 
-
 export function register<P, N extends string>(module: Module<P, N>) {
-    return <C extends {}, ModulesNeverIntersect = IfDef<KeysIntersection<C, { [K in N]: Module<P, N> }>, never, {}>>(
-        registry: Registry<C>,
+    return <
+        C extends NonNullable<unknown>,
+        ModulesNeverIntersect = IfDef<KeysIntersection<C, { [K in N]: Module<P, N> }>, never, NonNullable<unknown>>
+    >(
+        registry: Registry<C>
     ): ModulesNeverIntersect & Registry<C & { [K in N]: Module<P, N> }> => {
         if (module.moduleName in registry.modules) {
             throw new Error(`Key duplication detected between ${module.moduleName} - modules must have unique name`);
